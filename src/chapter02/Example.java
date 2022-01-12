8c5010c48d5ec305bec4c51da4636ae7c2786ddb
Traderved <thunderberry2020@gmail.com>
2022-01-12 12:28:51 +0200

    Initial commit

---
+++ src/chapter02/Example.java
@@ -0,0 +1,15 @@
+package chapter02;
+
+/*
+    Это простая программа на Java.
+    Присвоить исходному файлу имя "Example.java"
+    */
+public class Example {
+
+    // Эта программа начинается с вызова метода main()
+
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+
+}
---
+++ src/chapter02/Example2.java
@@ -0,0 +1,19 @@
+package chapter02;
+
+/*
+Это еще один короткий пример программы.
+Присвоить исходному файлу имя "Exaiaple2.java"
+*/
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num; // в этой строке кода объявляется переменная с именем num
+
+        num = 100; // в этой строке кода переменной num
+                   //присваивается значение 100
+        System.out.println ("Этo переменная num: " + num) ;
+        num = num * 2;
+        System.out.print("Знaчeниe переменной num * 2 равно ") ;
+        System.out.println(num) ;
+    }
+}
