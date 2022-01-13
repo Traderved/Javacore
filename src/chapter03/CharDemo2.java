d3cda43a5be6aeda2f38e348d93d9e22a2fe68bf
Traderved <thunderberry2020@gmail.com>
2022-01-13 10:30:49 +0200

    char example 2

---
+++ src/chapter03/CharDemo2.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 include  " + ch1);
+
+        ch1++; //увеличиваем на 1 значение переменной ch1
+        System.out.println("ch1 = " + ch1);
+        //the end
+
+    }
+}
+
