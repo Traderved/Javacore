3c4e7b57441a9ab3c2ac149c819c37d104340eac
Traderved <thunderberry2020@gmail.com>
2022-01-13 11:15:30 +0200

    динамическая инициализация

---
+++ src/chapter03/DynInit.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        // динамическая инициализация переменной с
+
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Гипотенуза c = " + c);
+
+    }
+}
