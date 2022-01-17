c47230a32615394824d4e3322f36d2f823ce4269
Traderved <thunderberry2020@gmail.com>
2022-01-17 12:44:11 +0200

    ternary

---
+++ src/chapter04/Ternary.java
@@ -0,0 +1,17 @@
+package chapter04;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+        int i, k;
+        i = 10;
+        k = i<0 ? -i : i; // если - то - иначе
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i<0 ? -i : i; // если - то - иначе
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+    }
+}
