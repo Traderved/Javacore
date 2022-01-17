533f648d42157f5b2bde6a534cb035414db3ff38
Traderved <thunderberry2020@gmail.com>
2022-01-17 14:31:04 +0200

    BreakLoop 4

---
+++ src/chapter05/BreakLoop4.java
@@ -0,0 +1,16 @@
+package chapter05;
+
+public class BreakLoop4 {
+    public static void main(String[] args) {
+
+        outer: for(int i = 0; i<3; i++) {
+            System.out.print("Prohod" + i + ": ");
+            for(int j = 0; j<100; j++) {
+                if(j ==10) break outer;
+                System.out.print(j + " ");
+            }
+            System.out.println("Eta stroka ne vivoditsa");
+        }
+        System.out.println("Cikli zaversheni");
+    }
+}
