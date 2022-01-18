d835e892b674d58f0eae1e652c7db79c621f7544
Traderved <thunderberry2020@gmail.com>
2022-01-18 10:59:05 +0200

    return

---
+++ src/chapter05/Return.java
@@ -0,0 +1,13 @@
+package chapter05;
+
+public class Return {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        System.out.println("Do vozvrata.");
+
+        if(t) return; // vozvrat i vizivaushiy kod
+
+        System.out.println("eto ne vipolniaetsia");
+    }
+}
